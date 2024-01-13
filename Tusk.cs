9a7d04a7f1914cce978235005bc7d22319eb708f
Vikrot Veresov <mymoonneey@yandex.ru>
2024-01-13 17:08:29 +0300

    Git init

---
+++ Tusk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Итоговая_контрольная_работа_по_основному_блоку
+{
+    internal class Tusk
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
