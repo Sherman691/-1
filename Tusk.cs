53a4a4967722b6eeed3bc1e5772e24084b02e84d
Vikrot Veresov <mymoonneey@yandex.ru>
2024-01-21 14:19:10 +0300

    Создал набор методов генерирующих предложение заданной длинны из случайных слов

--- Tusk.cs
+++ Tusk.cs
@@ -10,8 +10,50 @@ namespace Итоговая_контрольная_работа_по_основн
     {
         static void Main(string[] args)
         {
+            
+
+
+            int ReadInt(string text)
+            {
+                Console.WriteLine(text);
+                return Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Random random = new Random();
+            string GenerateRandomString(int length)
+            {
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            }
+
+
+
+            int numberOfWords = ReadInt("Введите длинну предложения: ");
+            int minWordsLenght = 1; //ReadInt("Введите минимальную длинну слова в предложении: ");
+            int maxWordsLenght = 5; // ReadInt("Введите максимальную длинну слова в предложении: ");
+
+            string GenerateRandomProposal(int lenght)
+            {
+                int rand = random.Next(minWordsLenght, maxWordsLenght + 1);
+                string proposal = GenerateRandomString(rand);
+
+                for (int i = 0; i <= lenght; i++)
+                {
+                    rand = random.Next(minWordsLenght, maxWordsLenght + 1);
+                    proposal = proposal + " " + GenerateRandomString(rand);
+                }
+
+                return proposal;
+            }
+
+            Console.WriteLine(GenerateRandomProposal(numberOfWords));
+
+
+
 
 
         }
+
     }
 }
